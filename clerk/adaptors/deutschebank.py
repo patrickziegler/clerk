7e2ba6de7d20ba168fafa51c4375c3eea70bc221
patrick <zipat@t-online.de>
2022-03-26 19:34:05 +0100

    Add deutsche bank scanner

--- clerk/adaptors/deutschebank.py
+++ clerk/adaptors/deutschebank.py
@@ -15,21 +15,83 @@
 
 from clerk.scanner import StatementScanner as scanner
 
+import csv
+import datetime
+import os
 import re
+import tempfile
 
 
 @scanner.register_filt("deutschebank")
 def deutschebank_filt(path):
+    """Return true if file with given path should be parsed with *_conv function"""
+
     pattern = "Kontoauszug.*pdf$"
-    return re.match(pattern, path) is not None
+    return re.match(pattern, path)
 
 
 @scanner.register_conv("deutschebank")
 def deutschebank_conv(path):
-    print(">>> " + path)
-    return [(3, 4, 5), (2, 3, 1), (1, 3, 4)]
+    """Yield tuples like (<date>, <description>, <amount>) as found in the given bank statement (pdf)"""
+
+    print(path)
+
+    with tempfile.NamedTemporaryFile("r") as tmp:
+        os.system("pdf2ps %s - | ps2pdf - - | pdftotext -raw -q - %s" % (path, tmp.name))
+        text = [line.rstrip("\n") for line in tmp.readlines()]
+
+    year = None
+    for i in range(len(text)):
+        if re.match("Kontoauszug vom .* bis .*", text[i]):
+            try:
+                year = str(int(text[i][-4:])) # via int to trigger ValueError for invalid input
+                break
+            except ValueError:
+                pass
+
+    if year is None:
+        return
+
+    i = 0
+    date, description, value = (None, "", None)
+    while i < len(text):
+        if text[i][:2] in ("- ", "+ ") and "," in text[i]:
+            try:
+                buf = float(text[i].replace(".", "").replace(",", ".").replace(" ", ""))
+                if date != None:
+                    yield (
+                        date,
+                        description.replace("Verwendungszweck/ Kundenreferenz", ""),
+                        value
+                    )
+                description = ""
+                value = buf
+            except ValueError:
+                pass
+        elif i < len(text) - 1:
+            try:
+                assert datetime.datetime.strptime(text[i] + year, "%d.%m.%Y")
+                date = datetime.datetime.strptime(text[i+1] + year, "%d.%m.%Y")
+                i += 1
+            except ValueError:
+                description += " " + text[i]
+        i += 1
 
 
 @scanner.register_conv_update("deutschebank")
 def deutschebank_conv_update(path):
-    raise NotImplementedError
+    """Yield tuples like (<date>, <description>, <amount>) as found in the given update file (csv)"""
+
+    with open(path, "r", encoding="ISO-8859-1") as fp:
+        reader = csv.reader(fp, delimiter=";", quotechar="\"")
+        for line in reader:
+            if len(line) < 13 or line[1] == "":
+                continue
+            try:
+                yield (
+                    datetime.datetime.strptime(line[0], "%d.%m.%Y"),
+                    line[4],
+                    float((line[-2] if line[-3] == "" else line[-3]).replace(".", "").replace(",", "."))
+                )
+            except (ValueError, IndexError):
+                continue
